e436f3b4ad2dec8c4cd79ceaff8372f7a47e0a3c
Anthony Vinicius <91100881+AnthonyVinicius@users.noreply.github.com>
2025-10-13 08:10:03 -0300

    fix

--- src/components/ItensTabelaChamado.vue
+++ src/components/ItensTabelaChamado.vue
@@ -3,32 +3,30 @@ import { Calendar1 } from 'lucide-vue-next'
 </script>
 
 <template>
-    <div
-        class="bg-gray-100 rounded-lg shadow-sm p-4 flex items-center justify-between hover:shadow-md transition-shadow duration-200">
+    <div class="bg-gray-100 rounded-lg shadow-sm py-8 px-4 flex items-center justify-between">
         <div class="flex items-start gap-3 w-1/3">
             <div class="mt-1">
                 <slot name="logo" />
             </div>
 
-            <div>
-                <h1 class="text-sm font-semibold text-gray-800 flex items-center gap-2">
+            <div class="gap-5 flex flex-col">
+                <h1 class="text-sm font-semibold flex items-center gap-2">
                     <slot name="title" />
                 </h1>
 
-                <p class="text-gray-500 text-xs">
+                <p class="text-xs">
                     Descrição:
                     <slot name="description" />
                 </p>
             </div>
         </div>
 
-        <div class="flex items-center gap-2 text-sm text-gray-700 w-1/3 justify-center">
+        <div class="flex items-center gap-2 text-sm  w-1/3 justify-center">
             <slot name="location" />
-            <span class="text-gray-400">•</span>
             <slot name="status" />
         </div>
 
-        <div class="flex items-center justify-end gap-5 w-1/3 text-sm text-gray-600">
+        <div class="flex items-center justify-end gap-5 w-1/3 text-sm ">
             <div class="flex items-center gap-1">
                 <Calendar1 class="w-4 h-4 text-green-800" />
                 <slot name="date" />
--- src/views/AdminDashboard.vue
+++ src/views/AdminDashboard.vue
@@ -1,77 +1,16 @@
 <script setup>
-import { MapPin, TextAlignJustify, Cog, Hand, CircleAlert } from 'lucide-vue-next'
+import { adminActions, recentCalls, chamadosPorPrioridade, chamadosPorStatus, chamadosPorLocal } from '../mock/MockDB'
 import BaseLayout from '../components/BaseLayout.vue'
 import Actions from '../components/Actions.vue'
 import ItensTabelaChamado from '../components/ItensTabelaChamado.vue'
 import BaseChart from '../components/BaseChart.vue';
-import { ref, onMounted } from 'vue';
-//import apiService from '../services/apiService';
 
-const adminActions = [
-    {
-        icon: MapPin,
-        title: 'Gerenciar Locais',
-        description: 'Adicionar novos locais e gerar QR Code',
-        color: 'text-green-800'
-    },
-    {
-        icon: Cog,
-        title: 'Tipos de Itens',
-        description: 'Gerenciar e cadastrar tipos de itens',
-        color: 'text-green-800'
-    },
-    {
-        icon: TextAlignJustify,
-        title: 'Todos os Chamados',
-        description: 'Visualizar e gerenciar chamados',
-        color: 'text-green-800'
-    }
-]
-
-const recentCalls = [
-    {
-        icon: Hand,
-        title: 'Secador de mãos',
-        description: 'Não liga',
-        location: 'Banheiro 2º Andar',
-        statusIcon: CircleAlert,
-        statusColor: 'text-red-500',
-        date: '21/08/2025',
-    },
-]
 
 // Dados dos gráficos
 //const chamadosPorPrioridade = ref({ labels: [], datasets: [] });
 //const chamadosPorStatus = ref({ labels: [], datasets: [] });
 //const chamadosPorLocal = ref({ labels: [], datasets: [] });
 
-// Dados de exemplo para os gráficos
-const chamadosPorPrioridade = ref({
-    labels: ['Alta', 'Média', 'Baixa'],
-    datasets: [{
-        label: 'Chamados',
-        data: [8, 12, 5],
-        backgroundColor: ['#ef4444', '#f59e0b', '#10b981']
-    }]
-});
-
-const chamadosPorStatus = ref({
-    labels: ['Aberto', 'Em Andamento', 'Concluído'],
-    datasets: [{
-        label: 'Chamados',
-        data: [10, 7, 8],
-        backgroundColor: ['#3b82f6','#f97316','#10b981']
-    }]
-});
-
-const chamadosPorLocal = ref({
-    labels: ['Lab. Informática', 'Biblioteca', 'Banheiros'],
-    datasets: [{
-        label: 'Chamados',
-        data: [6, 4, 15],
-        backgroundColor: ['#8b5cf6','#ec4899','#22d3ee']
-    }]
-});
 
 // Carrega os dados dos gráficos da API
 //onMounted(async () => {
@@ -117,31 +56,28 @@ const chamadosPorLocal = ref({
   <BaseLayout>
     <div class="space-y-8">
 
-      <!-- Cabeçalho -->
       <header>
         <h1 class="text-2xl font-bold text-ponto-if-green">Dashboard do Administrador</h1>
         <p class="text-gray-600">Bem-vindo ao sistema de manutenção do IFPE</p>
       </header>
 
-<!-- Gráficos centralizados -->
-<section class="flex flex-wrap justify-center gap-59">
-  <div class="w-90 h-90 bg-white p-10 rounded-lg shadow-md flex flex-col items-center justify-center">
-    <p class="text-lg font-semibold text-gray-700 text-center mb-2">Chamados Por Prioridade</p>
-    <BaseChart :chart-data="chamadosPorPrioridade" chart-type="doughnut" class="w-full h-64" />
-  </div>
+      <section class="flex flex-wrap justify-center gap-30">
+        <div class="w-90 h-90 bg-white p-10 rounded-lg shadow-md flex flex-col items-center justify-center">
+          <p class="text-lg font-semibold text-gray-700 text-center mb-2">Chamados Por Prioridade</p>
+          <BaseChart :chart-data="chamadosPorPrioridade" chart-type="doughnut" class="w-full h-64" />
+        </div>
 
-  <div class="w-90 h-90 bg-white p-10 rounded-lg shadow-md flex flex-col items-center justify-center">
-    <p class="text-lg font-semibold text-gray-700 text-center mb-2">Chamados Por Status</p>
-    <BaseChart :chart-data="chamadosPorStatus" chart-type="doughnut" class="w-full h-64" />
-  </div>
+        <div class="w-90 h-90 bg-white p-10 rounded-lg shadow-md flex flex-col items-center justify-center">
+          <p class="text-lg font-semibold text-gray-700 text-center mb-2">Chamados Por Status</p>
+          <BaseChart :chart-data="chamadosPorStatus" chart-type="doughnut" class="w-full h-64" />
+        </div>
 
-  <div class="w-90 h-90 bg-white p-10 rounded-lg shadow-md flex flex-col items-center justify-center">
-    <p class="text-lg font-semibold text-gray-700 text-center mb-2">Chamados Por Local</p>
-    <BaseChart :chart-data="chamadosPorLocal" chart-type="doughnut" class="w-full h-64" />
-  </div>
-</section>
+        <div class="w-90 h-90 bg-white p-10 rounded-lg shadow-md flex flex-col items-center justify-center">
+          <p class="text-lg font-semibold text-gray-700 text-center mb-2">Chamados Por Local</p>
+          <BaseChart :chart-data="chamadosPorLocal" chart-type="doughnut" class="w-full h-64" />
+        </div>
+      </section>
 
-      <!-- Cards de ações -->
       <section>
         <div class="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
           <Actions v-for="action in adminActions" :key="action.title">
@@ -154,10 +90,9 @@ const chamadosPorLocal = ref({
         </div>
       </section>
 
-      <!-- Últimos chamados -->
-      <div class="bg-white p-5 rounded-md shadow-sm">
+      <div class=" bg-white p-5 rounded-md shadow-sm">
         <div class="flex items-center mb-3">
-          <h2 class="text-lg font-semibold">Últimos Chamados Assumidos</h2>
+          <h2 class="text-lg font-semibold">Ultimos Chamados Assumidos</h2>
           <RouterLink to="/" class="ml-auto text-green-800 hover:underline">Ver todos</RouterLink>
         </div>
 
